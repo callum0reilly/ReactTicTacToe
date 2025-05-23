fa904428b92a07d2de911a2176670abf7d83da9e
callum0reilly <125283342+callum0reilly@users.noreply.github.com>
2025-05-23 23:42:37 +0100

    created a 3x3 square

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 
-export default function App() {
+export default function Square(){
   return (
+    <>
     <div>
-    <h1>Hello world</h1>
-    <h2> test</h2>
+    <button className = "square">1</button>
+    <button className = "square">2</button>
+    <button className = "square">3</button>
+    
     </div>
     
+    <div>
+    <button className = "square">4</button>
+    <button className = "square">5</button>
+    <button className = "square">6</button>
+    </div>
+
+    <div>
+    <button className = "square">7</button>
+    <button className = "square">8</button>
+    <button className = "square">9</button>
+    </div>
+    
+    </>
+    
   );
+  
+  
 }
 
 
